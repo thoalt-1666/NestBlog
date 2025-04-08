fdf7b240aca63e1c64050b4b4fb9a2eaea2b5461
huynv-3166 <110594805+huynv-3166@users.noreply.github.com>
2025-04-08 15:50:38 +0700

    remove unnecessary

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Version } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { API } from '../constants';
@@ -7,7 +7,6 @@ import { API } from '../constants';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   
-  @Version(API.VERSION)
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
