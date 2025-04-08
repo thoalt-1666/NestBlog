8631ef402b02b23847a34cb07d7046c9953f39f4
huynv-3166 <110594805+huynv-3166@users.noreply.github.com>
2025-04-08 15:52:29 +0700

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,7 @@ import { LoginDto } from './dto/login.dto';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
